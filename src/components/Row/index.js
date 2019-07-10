fc82a4e82b0a100d2d618398c183e44f89384527
Jexeones24 <jexeones@gmail.com>
2019-07-09 21:56:21 -0400

    adds styled components

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,19 +1,39 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
 
-const Row = ({ rowIdx, onClickHandler, row }) => (
-  <Fragment>
-    {row.map((cell, idx) => (
-      <div
-        className="cell"
-        key={idx}
-        onClick={() => onClickHandler(rowIdx, idx)}
-      >
-        <div className="mark">
-          {cell}
-        </div>
-      </div>
-    ))}
-  </Fragment>
-);
+const Row = ({ rowIdx, onClickHandler, row }) => {
+  const isDark = (cellIdx, rowIdx) => (rowIdx % 2 === 0 && cellIdx % 2 === 0) || (rowIdx % 2 !== 0 && cellIdx % 2 !== 0);
+
+  return (
+    <Fragment>
+      {row.map((cell, idx) => (
+        <Cell
+          key={idx}
+          dark={isDark(idx, rowIdx)}
+          onClick={() => onClickHandler(rowIdx, idx)}
+        >
+          <div className="mark">
+            {cell}
+          </div>
+        </Cell>
+      ))}
+    </Fragment>
+  );
+};
 
 export default Row;
+
+const CELL_WIDTH = '198px';
+
+const Cell = styled.div`
+  border: 1px solid;
+  background-color: ${({ dark }) => dark ? 'black' : '#ccc'};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  width: ${CELL_WIDTH};
+  height: ${CELL_WIDTH};
+  float: left;
+`;
